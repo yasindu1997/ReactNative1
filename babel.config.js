e73838ac7c98a6378389514ed92d3dbdbd2ba96d
yasindu1997 <yasindusathsaraoth@gmail.com>
2023-08-09 18:26:03 +0530

    fixed reanimated issue

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,12 @@ module.exports = {
       plugins: ['react-native-paper/babel'],
     },
   },
+  plugins: [
+    [
+      'react-native-reanimated/plugin', {
+        relativeSourceLocation: true,
+      },
+    ]
+  ],
 };
 
